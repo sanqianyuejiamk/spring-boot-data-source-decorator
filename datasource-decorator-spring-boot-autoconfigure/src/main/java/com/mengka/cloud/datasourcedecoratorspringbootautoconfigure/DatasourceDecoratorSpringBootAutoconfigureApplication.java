f2fd194247b91c13bf7a48a9ef4badc4e4cd8484
hyy044101331 <934734735@qq.com>
2018-02-21 21:11:24 +0800

    create

---
+++ datasource-decorator-spring-boot-autoconfigure/src/main/java/com/mengka/cloud/datasourcedecoratorspringbootautoconfigure/DatasourceDecoratorSpringBootAutoconfigureApplication.java
@@ -0,0 +1,12 @@
+package com.mengka.cloud.datasourcedecoratorspringbootautoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasourceDecoratorSpringBootAutoconfigureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasourceDecoratorSpringBootAutoconfigureApplication.class, args);
+	}
+}
---
+++ datasource-decorator-spring-boot-autoconfigure/src/test/java/com/mengka/cloud/datasourcedecoratorspringbootautoconfigure/DatasourceDecoratorSpringBootAutoconfigureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mengka.cloud.datasourcedecoratorspringbootautoconfigure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatasourceDecoratorSpringBootAutoconfigureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
