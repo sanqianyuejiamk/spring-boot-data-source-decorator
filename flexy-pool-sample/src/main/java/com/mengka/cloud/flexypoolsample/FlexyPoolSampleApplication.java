f54c8677fffb7a9ec1977c3eca1985f4b527d1a0
hyy044101331 <934734735@qq.com>
2018-02-21 22:04:36 +0800

    create

---
+++ flexy-pool-sample/src/main/java/com/mengka/cloud/flexypoolsample/FlexyPoolSampleApplication.java
@@ -0,0 +1,12 @@
+package com.mengka.cloud.flexypoolsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexyPoolSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexyPoolSampleApplication.class, args);
+	}
+}
